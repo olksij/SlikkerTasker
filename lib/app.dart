6b9bd1194463fd33213813dd1dc865df2f131b08
Alex Besida <pixel.coder7@gmail.com>
2020-08-27 23:38:36 +0300

    Added firestore listener and local db

--- lib/app.dart
+++ lib/app.dart
@@ -2,10 +2,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:package_info/package_info.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart';
 
 const double setsVersion = 0.1;
 
-bool signedIn; FirebaseUser user; Firestore firestoreDB = Firestore.instance;
+bool signedIn; FirebaseUser user; CollectionReference firestoreDB;
 List reminders; Map<String, dynamic> settings;
 
 Map<String, dynamic> getDefaults(PackageInfo packageInfo) => {
@@ -14,12 +16,14 @@ Map<String, dynamic> getDefaults(PackageInfo packageInfo) => {
 };
 
 Future<FirebaseUser> _signInWithCredential(googleAuth) async {
-   return (await FirebaseAuth.instance.signInWithCredential(
+   FirebaseUser user = (await FirebaseAuth.instance.signInWithCredential(
       GoogleAuthProvider.getCredential(
          accessToken: googleAuth.accessToken,
          idToken: googleAuth.idToken,
       )
    )).user; 
+   firestoreDB = Firestore.instance.collection(user.uid);
+   return user;
 }
 
 Future<bool> isSignedIn() async {
@@ -35,25 +39,34 @@ Future<bool> isSignedIn() async {
 }
 
 Future<FirebaseUser> signIn() async {
-   GoogleSignInAuthentication googleAuth = await (await GoogleSignIn().signIn()).authentication;
-   user =  await _signInWithCredential(googleAuth);
-   return user;
+   return await _signInWithCredential(
+      await (await GoogleSignIn().signIn()).authentication
+   );
 }
 
 Stream<QuerySnapshot> getFirestoreData() {
-
-   return firestoreDB.collection(user.uid).snapshots();
+   return firestoreDB.snapshots();
 }
 
 firestoreConnect() async {
-   var testSettings = await firestoreDB.collection(user.uid).document('.settings').get();
-   if (testSettings.exists) settings = testSettings.data;
-   else { 
+   if (!(await firestoreDB.document('.settings').get()).exists) { 
       settings = getDefaults(await PackageInfo.fromPlatform()); 
-      await firestoreDB.collection(user.uid).document('.settings').setData(settings);
+      await firestoreDB.document('.settings').setData(settings);
    }
+   Hive.init((await getApplicationDocumentsDirectory()).path);
+   var sttBox = await Hive.openBox('.settings');
+   firestoreDB.document('.settings').snapshots().listen((event) {
+      if (event.data != null) { sttBox.clear();
+         event.data.forEach((key, value) {
+            sttBox.put(key, value);
+         });
+      }
+   });
+   /*sttBox.watch().listen((event) async {
+      await firestoreDB.document('.settings').setData(sttBox.toMap());
+   });*/
 }
 
 newDoc(Map<String, dynamic> data) {
-   firestoreDB.collection(user.uid).document().setData(data);
+   firestoreDB.document().setData(data);
 }
