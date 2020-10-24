411d6ba0284df635e3414596da1625046513e878
Alex Besida <pixel.coder7@gmail.com>
2020-10-24 13:25:29 +0300

    Added time picker for day editor

--- lib/timetable/day_editor.dart
+++ lib/timetable/day_editor.dart
@@ -1,13 +1,25 @@
 import 'package:tasker/reusable/slikker.dart';
 
-class DayEditor extends StatelessWidget {
-   final Map day;
+class DayEditor extends StatefulWidget {
+   final Map oldDay;
    final double accent;
 
    const DayEditor({ 
-      this.day = const {'projects': []},
+      this.oldDay = const {'projects': []},
       this.accent = 240 
    });
+
+   @override _DayEditorState createState() => _DayEditorState();
+}
+
+class _DayEditorState extends State<DayEditor> {
+
+   Map newDay;
+
+   @override void initState() {
+      super.initState();
+      newDay = Map.from(widget.oldDay);
+   }
 
    @override Widget build(BuildContext context) {
       return SlikkerScaffold(
@@ -19,33 +31,37 @@ class DayEditor extends StatelessWidget {
                child: Row(
                   children: [
                      Container(width: 10),
-                     Text('When will you wake up?', style: TextStyle(
+                     Text('When your day starts?', style: TextStyle(
                         fontSize: 17, 
-                        color: HSVColor.fromAHSV(1, accent, 0.4, 0.4).toColor()
+                        color: HSVColor.fromAHSV(1, widget.accent, 0.4, 0.4).toColor()
                      )),
                      Expanded(child: Container()),
                      SlikkerCard(
                         accent: 240,
+                        onTap: () => showTimePicker(
+                           initialTime: TimeOfDay.now(),
+                           context: context,
+                        ).then((time) => setState(() => newDay['wakeup'] = time)),
                         isFloating: false,
                         borderRadius: BorderRadius.circular(8),
                         padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                        child: Text(day['wakeup'] ?? 'Tap!', style: TextStyle(
+                        child: Text(newDay['wakeup']?.format(context) ?? 'Tap!', style: TextStyle(
                            fontSize: 15, 
-                           color: HSVColor.fromAHSV(day['wakeup'] != null ? 1 : 0.5, accent, 0.4, 0.4).toColor()
+                           color: HSVColor.fromAHSV(newDay['wakeup'] != null ? 1 : 0.5, widget.accent, 0.4, 0.4).toColor()
                         )),
                      ),
                   ],
                ),
             )
          ),
-         content: day['wakeup'] == null ? SlikkerCard(
+         content: newDay['wakeup'] == null ? SlikkerCard(
             accent: 240,
             isFloating: false,
             child: Padding(
                padding: EdgeInsets.all(20),
                child: Text('To continue setuping your day, enter time when you wake up.', style: TextStyle(
                   fontSize: 16, 
-                  color: HSVColor.fromAHSV(0.7, accent, 0.4, 0.4).toColor()
+                  color: HSVColor.fromAHSV(0.7, widget.accent, 0.4, 0.4).toColor()
                ))
             ),
          ) : Container(),
